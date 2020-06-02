9d0d51e3db1c4c42ec6880db88b9f4b1ad2a0f77
gaurav <gauravsinha952@gmail.com>
2020-06-02 23:11:59 +0530

    added main.py file desc

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+"""
+This module runs the TensorFlow instances, train and evaluate the model.
+"""
+
+
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
