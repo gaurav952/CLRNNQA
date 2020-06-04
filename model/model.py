81c9b7fc6ca3561a509f7db02a7e39085c00df45
gaurav <gauravsinha952@gmail.com>
2020-06-04 23:14:41 +0530

    added model.py file description

--- model/model.py
+++ model/model.py
@@ -1,3 +1,7 @@
+"""
+This module constructs model functions that will be fed into the Estimator.
+"""
+
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
