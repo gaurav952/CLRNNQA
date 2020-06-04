b808d7cda9ccfe96e394210786b669599562931c
gaurav <gauravsinha952@gmail.com>
2020-06-04 23:13:04 +0530

    added functions get->story_length,sentence_length,loss,output and training optimizer to model.py :COMPLETE

--- model/model.py
+++ model/model.py
@@ -62,3 +62,70 @@ def model_fn(features, targets, mode, params, scope=None):
             tf.add_check_numerics_ops()
 
         return prediction, loss, train_op
+
+	def get_story_length(sequence, scope=None):
+    """
+    Find the actual length of a story that has been padded with zeros.
+    """
+    with tf.variable_scope(scope, 'StoryLength'):
+        used = tf.sign(tf.reduce_max(tf.abs(sequence), reduction_indices=[-1]))
+        tmp = tf.reduce_max(used, reduction_indices=[-1])
+        length = tf.cast(tf.reduce_sum(tmp, reduction_indices=[-1]), tf.int32)
+        return length
+
+
+def get_sentence_length(sequence, scope=None):
+    """
+    Find the actual length of a sentence that has been padded with zeros.
+    """
+    with tf.variable_scope(scope, 'SentenceLength'):  # 10 * 33 * 1O
+        used = tf.sign(tf.reduce_max(tf.abs(sequence), reduction_indices=[-1]))
+        length = tf.cast(tf.reduce_sum(used, reduction_indices=[-1]), tf.int32)
+        return length
+
+
+def get_output(last_state, vocab_size, activation=tf.nn.relu, initializer=None, scope=None):
+    with tf.variable_scope(scope, 'Output', initializer=initializer):
+        _, embedding_size = last_state.get_shape().as_list()
+
+        R = tf.get_variable('R', [embedding_size, vocab_size])
+        H = tf.get_variable('H', [embedding_size, embedding_size])
+
+        y = tf.matmul(activation(tf.matmul(last_state, H)), R)
+        return y
+
+
+def get_loss(output, labels, mode):
+    if mode == tf.contrib.learn.ModeKeys.INFER:
+        return None
+    return tf.contrib.losses.sparse_softmax_cross_entropy(output, labels)
+
+
+def training_optimizer(loss, params, mode):
+    if mode != tf.contrib.learn.ModeKeys.TRAIN:
+        return None
+
+    clip_gradients = params['clip_gradients']
+    learning_rate_init = params['learning_rate_init']
+    learning_rate_decay_rate = params['learning_rate_decay_rate']
+    learning_rate_decay_steps = params['learning_rate_decay_steps']
+
+    global_step = tf.contrib.framework.get_or_create_global_step()
+
+    learning_rate = tf.train.exponential_decay(
+        learning_rate=learning_rate_init,
+        decay_steps=learning_rate_decay_steps,
+        decay_rate=learning_rate_decay_rate,
+        global_step=global_step,
+        staircase=True)
+
+    tf.contrib.layers.summarize_tensor(learning_rate, tag='learning_rate')
+
+    train_op = tf.contrib.layers.optimize_loss(loss,
+                                               global_step=global_step,
+                                               learning_rate=learning_rate,
+                                               optimizer='Adam',
+                                               clip_gradients=clip_gradients
+                                               )
+
+    return train_op
