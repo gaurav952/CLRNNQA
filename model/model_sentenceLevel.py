23b94017a3b16391bb785485be5b0ddeb6af3e1b
gaurav <gauravsinha952@gmail.com>
2020-06-05 23:22:11 +0530

    sentence level model file: INCOMPLETE

---
+++ model/model_sentenceLevel.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+
+import tensorflow as tf
+
+
+def model_fn(features, targets, mode, params, scope=None):
+    embedding_size = params['embedding_size']
+    vocab_size = params['vocab_size']
+    hidden_units = params['hidden_units']
+    debug = params['debug']
+
+    story = features['story']
+    query = features['query']
+
+    batch_size = tf.shape(story)[0]
+
+    normal_initializer = tf.random_normal_initializer(stddev=0.1)
+    ones_initializer = tf.constant_initializer(1.0)
+
+    with tf.variable_scope(scope, 'Entity_Network', initializer=normal_initializer):
+        # Embeddings taking care of paddings
+        embedding_params = tf.get_variable('embedding_params', [vocab_size, embedding_size])
+        embedding_mask = tf.constant([0 if i == 0 else 1 for i in range(vocab_size)],
+                                     dtype=tf.float32,
+                                     shape=[vocab_size, 1])
+        embedding_params_masked = embedding_params * embedding_mask
+
+        story_embedding = tf.nn.embedding_lookup(embedding_params_masked, story)
+        query_embedding = tf.nn.embedding_lookup(embedding_params_masked, query)
