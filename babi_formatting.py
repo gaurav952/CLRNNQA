6df405ad86819ecd85a6f2f8187149975abfa2fa
gaurav <gauravsinha952@gmail.com>
2020-06-16 00:06:19 +0530

    reverting the COMMIT: two layer formatting and query removal.... to embedding the query and adding the vocabulary for answers + char-level for story and query

--- babi_formatting.py
+++ babi_formatting.py
@@ -25,18 +25,18 @@ PAD_TOKEN = '_PAD'
 PAD_ID = 0
 
 
+def tokenize(sentence):
+    """
+    Tokenize a string by splitting on non-word characters and stripping whitespace.
+    """
+    return [token.strip().lower() for token in re.split(SPLIT_RE, sentence) if token.strip()]
+
+
 def tokenize_char(sentence):
     """
     Tokenize a string by splitting on characters.
     """
     return list(sentence.lower())
-
-
-def tokenize_word(sentence):
-    """
-    Tokenize a string by splitting on non-word characters and stripping whitespace.
-    """
-    return [token.strip().lower() for token in re.split(SPLIT_RE, sentence) if token.strip()]
 
 
 def parse_stories(lines, only_supporting=False):
@@ -45,26 +45,28 @@ def parse_stories(lines, only_supporting=False):
     If only_supporting is True, only the sentences that support the answer are kept.
     """
     stories = []
-    story_char = []
-    story_word = []
+    story = []
     for line in lines:
         line = line.decode('utf-8').strip()
         nid, line = line.split(' ', 1)
         nid = int(nid)
         if nid == 1:
-            story_char = []
-            story_word = []
+            story = []
         if '\t' in line:
-            substory_char = [x for x in story_char if x]
-            substory_word = [x for x in story_word if x]
-            stories.append((substory_char, substory_word))
-            story_char.append('')
-            story_word.append('')
+            query, answer, supporting = line.split('\t')
+            query = tokenize_char(query)
+            if only_supporting:
+                # Only select the related substory
+                supporting = map(int, supporting.split())
+                substory = [story[i - 1] for i in supporting]
+            else:
+                # Provide all the substories
+                substory = [x for x in story if x]
+            stories.append((substory, query, answer))
+            story.append('')
         else:
-            sentence_char = tokenize_char(line)
-            sentence_word = tokenize_word(line)
-            story_char.append(sentence_char)
-            story_word.append(sentence_word)
+            sentence = tokenize_char(line)
+            story.append(sentence)
     return stories
 
 
@@ -73,8 +75,9 @@ def get_tokenizer_char(stories):
     Recover unique tokens as a vocab and map the tokens to ids.
     """
     tokens_all = []
-    for story_char, story_word in stories:
-        tokens_all.extend([token for sentence in story_word for token in sentence])
+    for story, query, answer in stories:
+        tokens_all.extend([token for sentence in story for token in sentence])
+        tokens_all.extend([token for sentence in query for token in sentence])
     vocab = [PAD_TOKEN] + sorted(set(tokens_all))
     token_to_id = {token: i for i, token in enumerate(vocab)}
     return token_to_id
@@ -86,8 +89,8 @@ def get_tokenizer_word(stories):
     """
     tokens_all = []
     for story, query, answer in stories:
-        tokens_all.extend([token for sentence in story for token in sentence] + query + [answer])
-    vocab = [PAD_TOKEN] + sorted(set(tokens_all))
+        tokens_all.extend([answer])
+    vocab = sorted(set(tokens_all))
     token_to_id = {token: i for i, token in enumerate(vocab)}
     return token_to_id
 
@@ -97,17 +100,17 @@ def tokenize_stories(stories, token_to_id_char, token_to_id_word):
     Convert all tokens into their unique ids.
     """
     story_ids = []
-    for story_char, story_word in stories:
-        story_char = [[token_to_id_char[token] for token in sentence] for sentence in story_char]
-        story_word = [[token_to_id_word[token] for token in sentence] for sentence in story_word]
-        story_ids.append((story_char, story_word))
-
+    for story, query, answer in stories:
+        story = [[token_to_id_char[token] for token in sentence] for sentence in story]
+        query = [token_to_id_char[token] for token in query]
+        answer = token_to_id_word[answer]
+        story_ids.append((story, query, answer))
     return story_ids
 
 
 def get_max_story_char_length(stories):
     char_total_length = []
-    for story, _ in stories:
+    for story, _, _ in stories:
         char_length = []
         for sentence in story:
             char_length.append(len(sentence))
@@ -115,32 +118,32 @@ def get_max_story_char_length(stories):
     return max(char_total_length)
 
 
-def get_max_story_word_length(stories):
+def get_max_story_word_length(stories, token_to_id):
     word_total_length = []
-    for _, story in stories:
+    for story, _, _ in stories:
         word_length = []
         for sentence in story:
-            word_length.append(len(sentence))
+            word_length.append(sentence.count(token_to_id[" "])+1)
         word_total_length.append(sum(word_length))
     return max(word_total_length)
 
 
-
-def pad_stories(stories, max_story_char_length, max_story_word_length):
+def pad_stories(stories, max_story_char_length, max_query_length):
     """
     Pad single vector stories, and queries to a consistence length.
     """
-    padded_story=[]
-    for story_char, story_word in stories:
-        story_flat_char = [token_id for sentence in story_char for token_id in sentence]
-        story_flat_word = [token_id for sentence in story_word for token_id in sentence]
-        for _ in range(max_story_char_length - len(story_flat_char)):
-            story_flat_char.append(PAD_ID)
-        for _ in range(max_story_word_length - len(story_flat_word)):
-            story_flat_word.append(PAD_ID)
-        padded_story.append([story_flat_char, story_flat_word])
-        assert len(story_flat_char) == max_story_char_length
-        assert len(story_flat_word) == max_story_word_length
+    padded_story = []
+    for story, query, answer in stories:
+        story_flat = [token_id for sentence in story for token_id in sentence]
+
+        for _ in range(max_story_char_length - len(story_flat)):
+            story_flat.append(PAD_ID)
+        for _ in range(max_query_length - len(query)):
+            query.append(PAD_ID)
+
+        padded_story.append([story_flat, query, answer])
+        assert len(story_flat) == max_story_char_length
+        assert len(query) == max_query_length
 
     return padded_story
 
@@ -154,12 +157,11 @@ def save_dataset(stories, path):
     Save the stories into TFRecords.
     """
     writer = tf.python_io.TFRecordWriter(path)
-    for story_char, story_word in stories:
-        story_flat = [token_id for sentence in story for token_id in sentence]
-
+    for story, query, answer in stories:
         features = tf.train.Features(feature={
-            'story_char': int64_features(story_char),
-            'story_word': int64_features(story_word),
+            'story': int64_features(story),
+            'query': int64_features(query),
+            'answer': int64_features([answer]),
         })
 
         example = tf.train.Example(features=features)
@@ -172,7 +174,26 @@ def main():
         os.makedirs(FLAGS.target_dir)
 
     filenames = [
-
+        'qa1_single-supporting-fact',
+        'qa2_two-supporting-facts',
+        'qa3_three-supporting-facts',
+        'qa4_two-arg-relations',
+        'qa5_three-arg-relations',
+        'qa6_yes-no-questions',
+        'qa7_counting',
+        'qa8_lists-sets',
+        'qa9_simple-negation',
+        'qa10_indefinite-knowledge',
+        'qa11_basic-coreference',
+        'qa12_conjunction',
+        'qa13_compound-coreference',
+        'qa14_time-reasoning',
+        'qa15_basic-deduction',
+        'qa16_basic-induction',
+        'qa17_positional-reasoning',
+        'qa18_size-reasoning',
+        'qa19_path-finding',
+        'qa20_agents-motivations',
     ]
 
     for filename in tqdm(filenames):
@@ -201,14 +222,19 @@ def main():
 
         token_to_id_char = get_tokenizer_char(stories_train + stories_test)
         token_to_id_word = get_tokenizer_word(stories_train + stories_test)
-        stories_token_train = tokenize_stories(stories_train, token_to_id)
-        stories_token_test = tokenize_stories(stories_test, token_to_id)
+
+        print(token_to_id_char)
+        print(token_to_id_word)
+
+        stories_token_train = tokenize_stories(stories_train, token_to_id_char, token_to_id_word)
+        stories_token_test = tokenize_stories(stories_test, token_to_id_char, token_to_id_word)
         stories_token_all = stories_token_train + stories_token_test
 
-        max_sentence_length = max([len(sentence) for story, _ in stories_token_all for sentence in story])
-        max_story_length = max([len(story) for story, _ in stories_token_all])
+        max_sentence_length = max([len(sentence) for story, _, _ in stories_token_all for sentence in story])
+        max_story_length = max([len(story) for story, _, _ in stories_token_all])
+        max_query_length = max([len(query) for _, query, _ in stories_token_all])
         max_story_char_length = get_max_story_char_length(stories_token_all)
-        max_story_word_length = get_max_story_word_length(stories_token_all)
+        max_story_word_length = get_max_story_word_length(stories_token_all, token_to_id_char)
         vocab_size_char = len(token_to_id_char)
         vocab_size_word = len(token_to_id_word)
 
@@ -217,11 +243,10 @@ def main():
                 'dataset_name': filename,
                 'dataset_size': dataset_size,
                 'max_sentence_char_length': max_sentence_length,
-                #'max_story_length': max_story_length,
-                #'max_query_length': max_query_length,
+                'max_story_length': max_story_length,
+                'max_query_char_length': max_query_length,
                 'max_story_char_length': max_story_char_length,
                 'max_story_word_length': max_story_word_length,
-                'max_story_length': max_story_length,
                 'vocab_size_char': vocab_size_char,
                 'vocab_size_word': vocab_size_word,
                 'tokens_char': token_to_id_char,
@@ -233,9 +258,9 @@ def main():
             }
             json.dump(metadata, f)
 
-        stories_pad_train = pad_stories(stories_token_train, max_story_char_length, max_story_word_length)
-        stories_pad_test = pad_stories(stories_token_test, max_story_char_length, max_story_word_length)
-        #print(stories_pad_train[0])
+        stories_pad_train = pad_stories(stories_token_train, max_story_char_length, max_query_length)
+        stories_pad_test = pad_stories(stories_token_test, max_story_char_length, max_query_length)
+
         save_dataset(stories_pad_train, dataset_path_train)
         save_dataset(stories_pad_test, dataset_path_test)
 
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 """
-This module runs the TensorFlow instances, train and evaluate the model.
+This module runs TensorFlow instances to train and evaluate the model.
 """
-
-
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
@@ -17,25 +15,24 @@ tf.reset_default_graph()
 tf.logging.set_verbosity(tf.logging.INFO)
 
 FLAGS = tf.app.flags.FLAGS
-#Hyper-parameters
+
+## Hyper-parameters
 tf.app.flags.DEFINE_integer('seed', 67, 'Random seed.')
-tf.app.flags.DEFINE_string('dataset', 'datasets/processed/qa1_single-supporting-fact_1k.json', 'Dataset path.')
-tf.app.flags.DEFINE_string('model_dir', 'logs/', 'Model directory.')
-tf.app.flags.DEFINE_integer('examples_per_epoch', 1000, 'Number of examples per epoch.')
-tf.app.flags.DEFINE_integer('batch_size', 100, 'Batch size.')
-tf.app.flags.DEFINE_integer('num_epochs', 400, 'Number of training epochs.')
+tf.app.flags.DEFINE_string('dataset_path', 'datasets/processed/qa1_single-supporting-fact_1k.json', 'Dataset path.')
+tf.app.flags.DEFINE_string('model_dir', 'logs_master/', 'Model directory.')
+tf.app.flags.DEFINE_integer('batch_size', 40, 'Batch size.')
+tf.app.flags.DEFINE_integer('num_epochs', 500, 'Number of training epochs.')
 tf.app.flags.DEFINE_integer('embedding_size', 100, 'Embedding size.')
-tf.app.flags.DEFINE_float('hidden_size', 500, 'GRU hidden size.')
-tf.app.flags.DEFINE_float('learning_rate', 5e-3, 'Base learning rate.')
+tf.app.flags.DEFINE_integer('hidden_size', 500, 'GRU hidden size.')
+tf.app.flags.DEFINE_float('learning_rate', 5e-2, 'Base learning rate.')
 tf.app.flags.DEFINE_float('clip_gradients', 40.0, 'Clip the global norm of the gradients to this value.')
-tf.app.flags.DEFINE_integer('early_stopping_rounds', 100, 'Number of epochs before early stopping.')
+tf.app.flags.DEFINE_integer('early_stopping_rounds', 200, 'Number of epochs before early stopping.')
 tf.app.flags.DEFINE_boolean('debug', False, 'Debug mode to enable more summaries and numerical checks.')
 
-print(FLAGS.dataset)
 
 def main(_):
-    ## TensorFlow processing batches
-    dataset = Data(FLAGS.dataset, FLAGS.batch_size)
+    ## Let TensorFlow take care of the batches
+    dataset = Data(FLAGS.dataset_path, FLAGS.batch_size)
     train_input_fn = dataset.get_input_fn('train', num_epochs=FLAGS.num_epochs, shuffle=True)
     eval_input_fn = dataset.get_input_fn('test', num_epochs=1, shuffle=False)
 
@@ -43,7 +40,7 @@ def main(_):
     params = {
         'vocab_size_char': dataset.vocab_size_char,
         'vocab_size_word': dataset.vocab_size_word,
-        'max_sentence_length': dataset.max_sentence_length,
+        'max_sentence_char_length': dataset.max_sentence_char_length,
         'max_story_length': dataset.max_story_length,
         'max_story_char_length': dataset.max_story_char_length,
         'max_story_word_length': dataset.max_story_word_length,
@@ -58,6 +55,7 @@ def main(_):
         'clip_gradients': FLAGS.clip_gradients,
         'debug': FLAGS.debug,
     }
+
     ## Configurations for the Estimator
     config = tf.contrib.learn.RunConfig(
         tf_random_seed=FLAGS.seed,
@@ -67,16 +65,20 @@ def main(_):
         keep_checkpoint_every_n_hours=1,
         log_device_placement=True)
 
-    dataset_name = os.path.splitext(os.path.basename(FLAGS.dataset))[0]
+    dataset_name = os.path.splitext(os.path.basename(FLAGS.dataset_path))[0]
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 
     model_dir = os.path.join(FLAGS.model_dir, dataset_name, str(timestamp))
+
     ## Building the Estimator
     estimator = tf.contrib.learn.Estimator(
         model_dir=model_dir,
         model_fn=model_fn,
         config=config,
         params=params)
+
+    # y = list(estimator.predict(input_fn=eval_input_fn))
+    # print("Predictions: {}".format(str(y)))
 
     validation_metrics = {
         "accuracy": tf.contrib.learn.MetricSpec(tf.contrib.metrics.streaming_accuracy)
@@ -88,6 +90,7 @@ def main(_):
         early_stopping_metric='loss',
         early_stopping_metric_minimize=True
     )]
+
     ## Building the Experiment that takes care of the training and evaluation loops
     experiment = tf.contrib.learn.Experiment(
         estimator,
@@ -95,12 +98,10 @@ def main(_):
         eval_input_fn,
         train_steps=None,
         eval_steps=None,
-        min_eval_frequency=1,
         eval_metrics=validation_metrics,
         train_monitors=validation_monitors)
 
     experiment.train_and_evaluate()
-    #experiment.train()
 
 if __name__ == '__main__':
     tf.app.run()
